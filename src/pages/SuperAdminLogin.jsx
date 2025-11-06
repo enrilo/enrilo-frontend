143cb78432922a39177ba0ea9f27158d0a1a26e5
aunshpatel <patelaunsh28@gmail.com>
2025-11-06 16:27:50 -0500

    Add login functionality to th page

--- src/pages/SuperAdminLogin.jsx
+++ src/pages/SuperAdminLogin.jsx
@@ -1,7 +1,54 @@
-import React from "react";
 import { TextField } from "@mui/material";
+import {useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {Link, useNavigate} from 'react-router-dom';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SuperAdminLogin() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData, 
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    dispatch(signInFailure(''));
+    console.log('inside handleSubmit');
+    
+    try{
+      dispatch(signInStart());
+      console.log('inside handleSubmit try block');
+      const res = await fetch('http://localhost:3000/api/super-admins/login', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(signInFailure(data.message));
+        return;
+      }
+      
+      dispatch(signInSuccess(data));
+      alert('Congratulations, you have signed in successfully. You will now be redirected to home page.');
+      navigate('/dashboard');
+    } catch(error){
+      console.log('inside handleSubmit catch block');
+      dispatch(signInFailure(error.message));
+      console.log(`error.message:${error.message}`);
+    }
+  }
+
   return (
     <main className="flex flex-1 justify-center items-center bg-gray-50 p-6">
       <div className="bg-white rounded-2xl shadow-lg p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
@@ -11,9 +58,9 @@ export default function SuperAdminLogin() {
         </h1>
 
         {/* Form */}
-        <form className="flex flex-col gap-4">
-          <TextField label="Email ID" variant="outlined" fullWidth />
-          <TextField label="Password" variant="outlined" type="password" fullWidth />
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+          <TextField label="Email ID" id="company_email" variant="outlined" onChange={handleChange} fullWidth />
+          <TextField label="Password" id="password" variant="outlined" type="password" onChange={handleChange} fullWidth />
 
           {/* Submit Button */}
           <div className="mt-6 flex justify-center">
