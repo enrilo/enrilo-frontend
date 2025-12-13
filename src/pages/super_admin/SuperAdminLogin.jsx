278503ead9f5bb65f7cc708986fb25ff99b92cec
Aunsh Patel <48309697+aunshpatel@users.noreply.github.com>
2025-12-13 13:25:05 -0500

    Update superAdmin userState reading error

--- src/pages/super_admin/SuperAdminLogin.jsx
+++ src/pages/super_admin/SuperAdminLogin.jsx
@@ -1,30 +1,147 @@
+// import { TextField } from "@mui/material";
+// import { useState, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Link, useNavigate } from 'react-router-dom';
+// import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice';
+
+// export default function SuperAdminLogin() {
+//   const [formData, setFormData] = useState({});
+//   const [showPassword, setShowPassword] = useState(false);
+//   const [modalMessage, setModalMessage] = useState("");  // For custom modal
+//   const [messageOpen, setMessageOpen] = useState(false); // Controls modal visibility
+//   const { loading, error } = useSelector((state) => state.user);
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const persistedRoot = JSON.parse(localStorage.getItem("persist:root"));
+//   // Parse the nested user slice
+//   const userState = JSON.parse(persistedRoot.user);
+
+//   useEffect(() =>{
+//       const redirectToDashboard = () => {
+//        if(userState.currentUser !== null) {
+//         navigate("/dashboard");
+//        }
+//       };
+  
+//       redirectToDashboard();
+//     }, []);
+
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.id]: e.target.value,
+//     });
+//   };
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     dispatch(signInFailure(""));
+
+//     try {
+//       dispatch(signInStart());
+//       const res = await fetch("http://localhost:3000/api/super-admins/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(formData),
+//         credentials: "include",
+//       });
+
+//       const data = await res.json();
+//       if (data.success === false) {
+//         dispatch(signInFailure(data.message));
+//         setModalMessage(data.message);
+//         setMessageOpen(true); // Show modal with error
+//         return;
+//       }
+
+//       dispatch(signInSuccess(data));
+//       setModalMessage("Congratulations, you have signed in successfully. You will now be redirected to home page.");
+//       setMessageOpen(true); // Show success modal
+
+//     } catch (error) {
+//       dispatch(signInFailure(error.message));
+//       setModalMessage(error.message);
+//       setMessageOpen(true); // Show error modal
+//     }
+//   };
+
+//   return (
+//     <main className="flex flex-1 justify-center items-center bg-gray-50 p-6">
+//       <div className="bg-white rounded-2xl shadow-lg p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
+//         {/* Title */}
+//         <h1 className="text-gray-900 font-semibold text-2xl sm:text-3xl text-center mb-6">
+//           Super Admin Login
+//         </h1>
+
+//         {/* Form */}
+//         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+//           <TextField label="Email ID" id="company_email" variant="outlined" onChange={handleChange} fullWidth />
+//           <TextField label="Password" id="password" variant="outlined" type={showPassword ? "text" : "password"} onChange={handleChange} fullWidth />
+
+//           {/* Show Password Checkbox */}
+//           <div className="flex items-center gap-2">
+//             <input type="checkbox" id="showPassword" checked={showPassword} onChange={() => setShowPassword(!showPassword)} className="cursor-pointer" />
+//             <label htmlFor="showPassword" className="text-gray-700">
+//               Show Password
+//             </label>
+//           </div>
+
+//           {/* Submit Button */}
+//           <div className="mt-6 flex justify-center">
+//             <button type="submit" className="w-full cursor-pointer sm:w-auto bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-8 py-2 rounded-md transition">
+//               Login
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+
+//       {/* Custom Modal */}
+//       {messageOpen && (
+//         <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
+//           <div className="bg-white text-[#334155] rounded-lg p-6 max-w-lg w-full shadow-xl text-center">
+//             <p className="mb-6 text-xl font-medium">{modalMessage}</p>
+//             <button className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-5 py-3 rounded-md w-28 transition cursor-pointer text-base" onClick={() => { setMessageOpen(false); if (modalMessage.includes("successfully")) { navigate("/dashboard"); } }}>
+//               OK
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </main>
+//   );
+// }
+
 import { TextField } from "@mui/material";
-import { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { signInStart, signInSuccess, signInFailure } from '../../redux/user/userSlice';
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../../redux/user/userSlice";
 
 export default function SuperAdminLogin() {
   const [formData, setFormData] = useState({});
   const [showPassword, setShowPassword] = useState(false);
-  const [modalMessage, setModalMessage] = useState("");  // For custom modal
-  const [messageOpen, setMessageOpen] = useState(false); // Controls modal visibility
-  const { loading, error } = useSelector((state) => state.user);
+  const [modalMessage, setModalMessage] = useState("");
+  const [messageOpen, setMessageOpen] = useState(false);
+
+  // ✅ SAFE Redux access
+  const { loading, error, currentUser } = useSelector(
+    (state) => state.user
+  );
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const persistedRoot = JSON.parse(localStorage.getItem("persist:root"));
-  // Parse the nested user slice
-  const userState = JSON.parse(persistedRoot.user);
 
-  useEffect(() =>{
-      const redirectToDashboard = () => {
-       if(userState.currentUser !== null) {
-        navigate("/dashboard");
-       }
-      };
-  
-      redirectToDashboard();
-    }, []);
+  // ✅ Redirect if already logged in
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/dashboard", { replace: true });
+    }
+  }, [currentUser, navigate]);
 
   const handleChange = (e) => {
     setFormData({
@@ -39,70 +156,103 @@ export default function SuperAdminLogin() {
 
     try {
       dispatch(signInStart());
-      const res = await fetch("http://localhost:3000/api/super-admins/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-        credentials: "include",
-      });
+
+      const res = await fetch(
+        "http://localhost:3000/api/super-admins/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+          credentials: "include",
+        }
+      );
 
       const data = await res.json();
+
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         setModalMessage(data.message);
-        setMessageOpen(true); // Show modal with error
+        setMessageOpen(true);
         return;
       }
 
       dispatch(signInSuccess(data));
-      setModalMessage("Congratulations, you have signed in successfully. You will now be redirected to home page.");
-      setMessageOpen(true); // Show success modal
-
+      setModalMessage(
+        "Congratulations, you have signed in successfully. You will now be redirected to home page."
+      );
+      setMessageOpen(true);
     } catch (error) {
       dispatch(signInFailure(error.message));
       setModalMessage(error.message);
-      setMessageOpen(true); // Show error modal
+      setMessageOpen(true);
     }
   };
 
   return (
     <main className="flex flex-1 justify-center items-center bg-gray-50 p-6">
       <div className="bg-white rounded-2xl shadow-lg p-6 sm:p-8 w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl">
-        {/* Title */}
         <h1 className="text-gray-900 font-semibold text-2xl sm:text-3xl text-center mb-6">
           Super Admin Login
         </h1>
 
-        {/* Form */}
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-          <TextField label="Email ID" id="company_email" variant="outlined" onChange={handleChange} fullWidth />
-          <TextField label="Password" id="password" variant="outlined" type={showPassword ? "text" : "password"} onChange={handleChange} fullWidth />
+          <TextField
+            label="Email ID"
+            id="company_email"
+            variant="outlined"
+            onChange={handleChange}
+            fullWidth
+          />
 
-          {/* Show Password Checkbox */}
+          <TextField
+            label="Password"
+            id="password"
+            variant="outlined"
+            type={showPassword ? "text" : "password"}
+            onChange={handleChange}
+            fullWidth
+          />
+
           <div className="flex items-center gap-2">
-            <input type="checkbox" id="showPassword" checked={showPassword} onChange={() => setShowPassword(!showPassword)} className="cursor-pointer" />
+            <input
+              type="checkbox"
+              id="showPassword"
+              checked={showPassword}
+              onChange={() => setShowPassword(!showPassword)}
+              className="cursor-pointer"
+            />
             <label htmlFor="showPassword" className="text-gray-700">
               Show Password
             </label>
           </div>
 
-          {/* Submit Button */}
           <div className="mt-6 flex justify-center">
-            <button type="submit" className="w-full cursor-pointer sm:w-auto bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-8 py-2 rounded-md transition">
-              Login
+            <button
+              type="submit"
+              disabled={loading}
+              className="w-full cursor-pointer sm:w-auto bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-8 py-2 rounded-md transition disabled:opacity-60"
+            >
+              {loading ? "Logging in..." : "Login"}
             </button>
           </div>
         </form>
       </div>
 
-      {/* Custom Modal */}
       {messageOpen && (
         <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
           <div className="bg-white text-[#334155] rounded-lg p-6 max-w-lg w-full shadow-xl text-center">
             <p className="mb-6 text-xl font-medium">{modalMessage}</p>
-            <button className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-5 py-3 rounded-md w-28 transition cursor-pointer text-base" onClick={() => { setMessageOpen(false); if (modalMessage.includes("successfully")) { navigate("/dashboard"); } }}>
+            <button
+              className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 font-semibold px-5 py-3 rounded-md w-28 transition cursor-pointer text-base"
+              onClick={() => {
+                setMessageOpen(false);
+                if (modalMessage.includes("successfully")) {
+                  navigate("/dashboard", { replace: true });
+                }
+              }}
+            >
               OK
             </button>
           </div>
