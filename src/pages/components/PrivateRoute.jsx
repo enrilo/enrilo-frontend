9b2e3f7bfcb668856ea6d0817de90af8911c2e6e
Aunsh Patel <48309697+aunshpatel@users.noreply.github.com>
2025-12-13 13:24:30 -0500

    Update route security to redirect even misspelled routes

--- src/pages/components/PrivateRoute.jsx
+++ src/pages/components/PrivateRoute.jsx
@@ -1,8 +1,23 @@
-import { useSelector } from 'react-redux';
-import { Outlet, Navigate } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { Outlet, Navigate } from 'react-router-dom';
+
+// export default function PrivateRoute() {
+//   const {currentUser} = useSelector((state) => state.user);
+  
+//   return currentUser ? <Outlet /> : <Navigate to="/super-admin-login" />
+// }
+import { useSelector } from "react-redux";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
 
 export default function PrivateRoute() {
-  const {currentUser} = useSelector((state) => state.user);
-  
-  return currentUser ? <Outlet /> : <Navigate to="/login" />
+  const currentUser = useSelector((state) => state.user?.currentUser);
+  const location = useLocation();
+
+  if (!currentUser) {
+    return (
+      <Navigate to="/" replace state={{ from: location.pathname }} />
+    );
+  }
+
+  return <Outlet />;
 }
