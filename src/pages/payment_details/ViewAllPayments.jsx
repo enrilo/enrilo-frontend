89f0f1824e8223e22a7391de81a40a657b03b2d8
Aunsh Patel <48309697+aunshpatel@users.noreply.github.com>
2025-12-27 05:11:32 -0500

    Complete full page functionality

--- src/pages/payment_details/ViewAllPayments.jsx
+++ src/pages/payment_details/ViewAllPayments.jsx
@@ -1,9 +1,273 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { styled } from "@mui/system";
+import TablePagination from "@mui/material/TablePagination";
+import { getStorage, ref, deleteObject } from "firebase/storage";
+import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
+import LastPageRoundedIcon from "@mui/icons-material/LastPageRounded";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 
 export default function ViewAllPayments() {
+  // const { loading } = useSelector((state) => state.user);
+  // const [formData, setFormData] = useState({
+  //   consultancy_id: "",
+  //   consultancy_name: "",
+  //   rate: 0,
+  //   duration_in_months: 0,
+  //   subtotal: 0,
+  //   is_discount_available: false,
+  //   discount_amount: 0,
+  //   net_total: 0,
+  //   gst_amount: 0,
+  //   grand_total: 0,
+  //   payment_status: "pending",
+  //   payment_received: 0,
+  //   pending_payment: 0,
+  //   from_date: null,
+  //   to_date: null,
+  // });
+
+  const [formData, setFormData] = useState([]);
+  const [currentUserID, setCurrentUserID] = useState("");
+  const [pageLoading, setPageLoading] = useState(false);
+  const [allowWriteAccess, setAllowWriteAccess] = useState(false);
+  const [showConfirmDelete, setShowConfirmDelete] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showDeleting, setShowDeleting] = useState(false);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const persistedRoot = JSON.parse(localStorage.getItem("persist:root"));
+  const userState = JSON.parse(persistedRoot.user);
+  const token = userState.currentUser?.data?.accessToken;
+  const loggedInUserID = userState.currentUser?.data?.id;
+  const role = userState.currentUser?.data?.role;
+
+  useEffect(() => {
+    const fetchAllPayments = async () => {
+      try {
+        setPageLoading(true);
+        setCurrentUserID(userState.currentUser?.data?.id);
+
+        const res = await fetch(`http://localhost:3000/api/payment-detail/`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        const data = await res.json();
+        if (!data.success) {
+          setFormData([]);
+          setPageLoading(false);
+          return;
+        }
+        
+        
+        setFormData(data.data.paymentDetails);
+
+        // Fetch write access
+        const accessRes = await fetch(
+          `http://localhost:3000/api/access-tokens/access-token-by-super-admin-id/${loggedInUserID}`,
+          { method: "GET", headers: { "Content-Type": "application/json" } }
+        );
+        const accessData = await accessRes.json();
+        if (accessData.success) setAllowWriteAccess(accessData.data.accessToken.allow_write_access);
+
+        setPageLoading(false);
+      } catch (error) {
+        console.log(`Error:${error}`)
+        setPageLoading(false);
+      }
+    };
+    fetchAllPayments();
+  }, []);
+
+  const paginatedData = rowsPerPage > 0 ? formData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : formData;
+
+  const handleChangePage = (event, newPage) => setPage(newPage);
+
+  const handleChangeRowsPerPage = (event) => { setRowsPerPage(parseInt(event.target.value, 10)); setPage(0); };
+
+  const CustomTablePagination = styled(TablePagination)` & .MuiTablePagination-toolbar { display: flex; justify-content: space-between; align-items: center; font-size: 14px; background-color: #f9fafb; flex-wrap: wrap; } & .MuiTablePagination-selectLabel, & .MuiTablePagination-input, & .MuiTablePagination-displayedRows { font-size: 14px; }`;
+  
   return (
-    <div>
-      
-    </div>
-  )
+    <main className="flex-1 overflow-y-auto p-6">
+      <div className="p-4 sm:p-6">
+        <div className="bg-white rounded-2xl shadow p-4 sm:p-6 max-w-7xl mx-auto">
+          {/* 🔹 TABLE */}
+          <div className="overflow-x-auto rounded-lg">
+            {allowWriteAccess && role === "admin" && (
+              <div className="flex justify-end mb-5">
+                <Link to={`/add-new-payment`} className="flex flex-row justify-center">
+                  <button className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 cursor-pointer px-2 sm:px-3 py-1 sm:py-1.5 rounded text-sm sm:text-[17px] font-semibold transition">
+                    Add New Payment
+                  </button>
+                </Link>
+              </div>
+            )}
+            <table className="min-w-full text-sm md:text-[17px] text-left border-collapse rounded-lg">
+              <thead className="bg-gray-100 text-gray-700 uppercase rounded-lg">
+                <tr>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-8 md:w-12">#</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-40 md:w-56">Consultancy Name</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-44 md:w-50">Plan Duration</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-32 md:w-40">Payment Status</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-44 md:w-40">Total Payment</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-22 md:w-20">Payment Received</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-36 md:w-70">Pending Payment</th>
+                  <th className="px-3 py-2 md:px-4 md:py-3 text-center w-36 md:w-70">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {paginatedData.map((payment, index) => (
+                  <tr key={payment._id} className="border-b hover:bg-gray-50 transition text-gray-800">
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center font-medium w-8 md:w-12">
+                      {page * rowsPerPage + index + 1}
+                    </td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-40 md:w-56">{payment.consultancy_name}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-44 md:w-50">{payment.duration_in_months}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-32 md:w-40">{payment.payment_status}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-44 md:w-40">{payment.grand_total}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-22 md:w-20 capitalize">{payment.payment_received}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-22 md:w-20 capitalize">{payment.pending_payment}</td>
+                    <td className="px-3 py-2 md:px-4 md:py-3 text-center w-36 md:w-70">
+                      <div className="flex justify-center gap-1 sm:gap-4 flex-wrap">
+                        <Link to={`/view-super-admin/${payment._id}`}>
+                          <button className="bg-slate-500 hover:bg-slate-600 text-white px-2 sm:px-3 py-1 sm:py-1.5 rounded text-sm sm:text-[17px] font-semibold transition cursor-pointer">View</button>
+                        </Link>
+                        {allowWriteAccess && role === "admin" && (
+                          <>
+                            <Link to={`/edit-super-admin/${payment._id}`}>
+                              <button className="bg-[#1E293B] hover:bg-[#334155] text-white px-2 sm:px-3 py-1 sm:py-1.5 rounded text-sm sm:text-[17px] font-semibold transition cursor-pointer">Edit</button>
+                            </Link>
+                            <button onClick={() => confirmDelete(payment._id)} className="bg-red-700 hover:bg-red-600 text-white px-2 sm:px-3 py-1 sm:py-1.5 rounded text-sm sm:text-[17px] font-semibold transition cursor-pointer">
+                              Delete
+                            </button>
+                          </>
+                        )}
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+
+              {/* Pagination Footer */}
+              <tfoot>
+                <tr>
+                  <td colSpan={7}>
+                    <div className="flex items-center justify-between mt-4 px-3 py-2 bg-gray-50 text-sm md:text-base">
+                      <div className="flex items-center gap-2">
+                        <span>Rows per page:</span>
+                        <select className="border rounded px-2 py-1" value={rowsPerPage} onChange={(e) => { const value = parseInt(e.target.value, 10); setRowsPerPage(value); setPage(0); }}>
+                          {[5, 10, 25, -1].map((num) => (
+                            <option key={num} value={num}>
+                              {num === -1 ? "All" : num}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+
+                      <div style={{ flex: 1 }} />
+
+                      <div className="flex items-center gap-2">
+                        <span>
+                          {rowsPerPage === -1 ? `1–${formData.length} of ${formData.length}` : `${Math.min(page * rowsPerPage + 1, formData.length)}–${Math.min( (page + 1) * rowsPerPage, formData.length )} of ${formData.length}`}
+                        </span>
+                        <button onClick={() => setPage(0)} disabled={page === 0 || rowsPerPage === -1} className="px-2 py-1 border rounded disabled:opacity-40">
+                          <FirstPageRoundedIcon fontSize="small" />
+                        </button>
+                        <button onClick={() => setPage((p) => Math.max(p - 1, 0))} disabled={page === 0 || rowsPerPage === -1} className="px-2 py-1 border rounded disabled:opacity-40">
+                          <ChevronLeftRoundedIcon fontSize="small" />
+                        </button>
+                        <button onClick={() => setPage((p) => Math.min(p + 1, Math.ceil(formData.length / rowsPerPage) - 1))} disabled={page >= Math.ceil(formData.length / rowsPerPage) - 1 || rowsPerPage === -1} className="px-2 py-1 border rounded disabled:opacity-40">
+                          <ChevronRightRoundedIcon fontSize="small" />
+                        </button>
+                        <button onClick={() => setPage(Math.ceil(formData.length / rowsPerPage) - 1)} disabled={page >= Math.ceil(formData.length / rowsPerPage) - 1 || rowsPerPage === -1} className="px-2 py-1 border rounded disabled:opacity-40">
+                          <LastPageRoundedIcon fontSize="small" />
+                        </button>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+
+          {/* DELETE CONFIRM MODAL */}
+          {showConfirmDelete && (
+            <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
+              <div className="bg-white text-[#334155] rounded-lg p-6 w-80 shadow-xl text-center">
+                <p className="text-center font-medium text-xl mb-5">Are you sure you want to delete this payment detail?</p>
+                <div className="flex justify-center gap-4">
+                  <button className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 border-2 px-4 py-2 rounded-md w-24 transition cursor-pointer" onClick={handleDeleteConfirmed}>
+                    Yes
+                  </button>
+                  <button className="bg-gray-300 border-2 px-4 py-2 rounded-md w-24 hover:bg-gray-400 transition cursor-pointer" onClick={() => setShowConfirmDelete(false)}>
+                    No
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {/* SUCCESS MODAL */}
+          {showSuccess && (
+            <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
+              <div className="bg-white text-[#334155] rounded-lg p-6 w-80 shadow-xl text-center">
+                <p className="text-center font-medium text-xl mb-5">Payment Detail has been deleted!</p>
+                <div className="flex justify-center gap-4">
+                  <button className="bg-[#1E293B] hover:bg-[#334155] text-yellow-300 border-2 px-4 py-2 rounded-md w-24 transition cursor-pointer" onClick={() => { setShowSuccess(false); window.location.reload(true); }}>
+                    OK
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {showDeleting && (
+            <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
+              <div className="bg-white text-[#334155] rounded-lg p-6 w-80 shadow-xl text-center">
+                <div className="text-xl font-semibold flex justify-center items-center gap-3">
+                  <div className="w-20 h-20">
+                    <svg className="w-full h-full animate-spin text-yellow-400" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true">
+                      <text x="50" y="68" textAnchor="middle" fontFamily="Inter, ui-sans-serif, system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial" fontSize="100" fontWeight="700" fill="currentColor">
+                        e
+                      </text>
+                    </svg>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-xl font-semibold mb-2">Deleting...</p>
+                    <p className="text-[#334155]">Please hold on! The payment detail is being deleted.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {/* LOADING MODAL */}
+          {pageLoading && (
+            <div className="fixed inset-0 backdrop-blur-md flex justify-center items-center z-50">
+              <div className="bg-white text-[#334155] rounded-lg p-6 w-80 shadow-xl text-center">
+                <div className="text-xl font-semibold flex justify-center items-center gap-3">
+                  <div className="w-20 h-20">
+                    <svg className="w-full h-full animate-spin text-yellow-400" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden="true">
+                      <text x="50" y="68" textAnchor="middle" fontFamily="Inter, ui-sans-serif, system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', Arial" fontSize="100" fontWeight="700" fill="currentColor">
+                        e
+                      </text>
+                    </svg>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="text-xl font-semibold mb-2">Loading...</p>
+                    <p className="text-[#334155]">Please wait while we load the all payment details.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </main>
+  );
 }
