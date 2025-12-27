68abdf0aac3ed46feab441390cde2469bebf865a
Aunsh Patel <48309697+aunshpatel@users.noreply.github.com>
2025-12-27 04:37:21 -0500

    Add LocalizationProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,17 @@
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { store } from './redux/store.js';
+// import App from './App.jsx';
+// import './index.css';
+
+// createRoot(document.getElementById('root')).render(
+//   <Provider store={store}>
+//     <StrictMode>
+//       <App />
+//     </StrictMode>
+//   </Provider>
+// );
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -5,10 +19,15 @@ import { store } from './redux/store.js';
 import App from './App.jsx';
 import './index.css';
 
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StrictMode>
-      <App />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <App />
+      </LocalizationProvider>
     </StrictMode>
   </Provider>
 );
